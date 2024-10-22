0497ecda1cbcb6370f0833c4cb3542d51635c452
mk <v.a.stekolschikov@gmail.com>
2024-10-22 19:32:19 +0100

    fixed build

--- src/services/ip.service.ts
+++ src/services/ip.service.ts
@@ -1,7 +1,11 @@
 import SxGeo from 'sxgeo-node';
 import path from 'path';
+console.log(process.env.NODE_ENV )
+const dataFilePath = process.env.NODE_ENV === 'production'
+    ? path.resolve(__dirname, './data/SxGeoCity.dat')  // Путь для production (после сборки)
+    : path.resolve(__dirname, '../data/SxGeoCity.dat');  // Путь для разработки
 
-const sxgeo = new SxGeo(path.resolve(__dirname, '../data/SxGeoCity.dat'));
+const sxgeo = new SxGeo(dataFilePath);
 
 export const getLocationByIp = (ip: string) => {
     return sxgeo.getCityFull(ip); // Вернет данные местоположения или false
