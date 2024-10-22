17f4279a28d392805ee95be97914ed63f6f46b45
mk <v.a.stekolschikov@gmail.com>
2024-10-22 20:45:55 +0100

    test CI/CD

--- src/validation/user.validation.ts
+++ src/validation/user.validation.ts
@@ -1,7 +1,4 @@
 import Joi from 'joi';
 
 export const userSchema = Joi.object({
-    name: Joi.string().min(3).required().messages({
-        'string.min': 'Name must be at least 3 characters long',
-    })
 });
